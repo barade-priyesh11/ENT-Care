0cc94f8fb0b68a571c80b3fd97ee97e2e576ba05
Pranavvishwakarma <pranav213234@gmail.com>
2024-03-15 14:06:55 +0530

    Code for Deployment

---
+++ src/JS/Ear_Page.js
@@ -0,0 +1,34 @@
+
+  $(document).ready(function(){
+  $('#navbarDropdownServices').hover(function(){
+    $(this).attr('aria-expanded', 'true');
+    $(this).parent('.nav-item.dropdown').addClass('show');
+    $(this).next('.dropdown-menu').addClass('show');
+
+    // Hide all dropdown items initially
+    $('.dropdown-item').hide();
+
+    // Show each dropdown item one by one
+    $('.dropdown-item').each(function(i){
+      $(this).delay(100 * i).fadeIn(500);
+    });
+
+  }, function(){
+    // This function is for mouse out
+  });
+
+  // Close the dropdown when mouse leaves the dropdown area
+  $('.nav-item.dropdown').mouseleave(function(){
+    $(this).removeClass('show');
+    $(this).find('.dropdown-menu').removeClass('show');
+    $(this).find('#navbarDropdownServices').attr('aria-expanded', 'false');
+    $('.dropdown-item').show(); // Reset to show all items for next hover
+  });
+});
+
+
+
+ 
+  
+
+ 
